20e7063700f28a4ebecb15f614add90c89b43822
Jacquelyn Mitchell <jac.nmitchell@gmail.com>
2021-11-11 17:40:48 -0500

    first commit

---
+++ FWDungeon/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FWDungeon
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public int Health { get; set; }
+        public int Speed { get; set; }
+
+        public Player()
+        {
+            this.Health = 100;
+            this.Damage = 10;
+            this.Speed = 15;
+            //setting default values for player
+        }
+
+
+    }
+}
+
---
+++ FWDungeon/UI/IConsole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FWDungeon.UI
+{
+    public interface IConsole
+    {
+        ConsoleKeyInfo ReadKey();
+        string ReadLine();
+        void Clear();
+
+        void WriteLine(string s);
+        void WriteLine(object o);
+
+    }
+}
